955505cfba021c12ebf23f2fea88366dffd4fadc
Mayboyzz <mayboyz99@gmail.com>
2024-11-12 17:43:11 -0600

    Refactor(Channel Routes): Refactored all route validations and added error handlers to the routes

--- backend/app/api/channel_routes.py
+++ backend/app/api/channel_routes.py
@@ -11,26 +11,23 @@ def get_channel(id):
     """
     Get a channel by it's ID
     """
-    channel = Channel.query.get(id)
-
-    if not current_user.is_authenticated:
-        return {"errors": {"message": "Unauthorized"}}, 401
-
-    # Check if user is member of server that owns this channel
-    server_member = (
-        ServerMember.query.filter(
-            ServerMember.server_id == channel.server_id,
-            ServerMember.user_id == current_user.id,
-        ).first()
-        if channel
-        else None
-    )
-    if not server_member:
-        return {"errors": {"message": "Unauthorized"}}, 401
-    if channel:
-        return channel.to_dict(), 200
+    if current_user.is_authenticated:
+        channel = Channel.query.get(id)
+        if channel:
+            server_member = ServerMember.query.filter(
+                ServerMember.server_id == channel.server_id,
+                ServerMember.user_id == current_user.id,
+            ).first()
+            if server_member:
+                return channel.to_dict(), 200
+            else:
+                return {
+                    "errors": {"message": "You are not a member of this server"}
+                }, 401
+        else:
+            return {"errors": {"message": "Channel not found"}}, 404
     else:
-        return {"errors": {"message": "Channel not found"}}, 404
+        return {"errors": {"message": "Unauthorized"}}, 401
 
 
 @channel_routes.route("/<int:id>", methods=["PUT"])
@@ -40,31 +37,33 @@ def update_channel(id):
     """
     form = ChannelForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
-    if form.validate_on_submit():
-        channel = Channel.query.get(id)
-        if channel:
-            # Check if user is the owner of the server that owns this channel
-            server_member = ServerMember.query.filter(
-                ServerMember.server_id == channel.server_id,
-                ServerMember.user_id == current_user.id,
-            ).first()
-            if not server_member.is_owner:
-                return {
-                    "errors": {
-                        "message": "You must be the owner of the server to update this channel"
-                    }
-                }, 401
-
-            if form.data["name"] is not None:
-                channel.name = form.data["name"]
-            if form.data["visibility"] is not None:
-                channel.visibility = form.data["visibility"]
-            db.session.commit()
-            return channel.to_dict(), 200
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
+            channel = Channel.query.get(id)
+            if channel:
+                server_member = ServerMember.query.filter(
+                    ServerMember.server_id == channel.server_id,
+                    ServerMember.user_id == current_user.id,
+                ).first()
+                if server_member.is_owner:
+                    if form.data["name"] is not None:
+                        channel.name = form.data["name"]
+                    if form.data["visibility"] is not None:
+                        channel.visibility = form.data["visibility"]
+                    db.session.commit()
+                    return channel.to_dict(), 200
+                else:
+                    return {
+                        "errors": {
+                            "message": "You must be the owner of the server to update this channel"
+                        }
+                    }, 401
+            else:
+                return {"errors": {"message": "Channel not found"}}, 404
         else:
-            return {"errors": {"message": "Channel not found"}}, 404
+            return {"errors": form.errors}, 400
     else:
-        return {"errors": form.errors}, 400
+        return {"errors": {"message": "Unauthorized"}}, 401
 
 
 @channel_routes.route("/<int:id>", methods=["DELETE"])
@@ -72,23 +71,27 @@ def delete_channel(id):
     """
     Delete a channel by it's ID
     """
-    channel = Channel.query.get(id)
-    if channel:
-        server_member = ServerMember.query.filter(
-            ServerMember.server_id == channel.server_id,
-            ServerMember.user_id == current_user.id,
-        ).first()
-        if not server_member.is_owner:
-            return {
-                "errors": {
-                    "message": "You must be the owner of the server to delete this channel"
-                }
-            }, 401
-        db.session.delete(channel)
-        db.session.commit()
-        return {"message": "Channel successfully deleted"}, 200
+    if current_user.is_authenticated:
+        channel = Channel.query.get(id)
+        if channel:
+            server_member = ServerMember.query.filter(
+                ServerMember.server_id == channel.server_id,
+                ServerMember.user_id == current_user.id,
+            ).first()
+            if server_member.is_owner:
+                db.session.delete(channel)
+                db.session.commit()
+                return {"message": "Channel successfully deleted"}, 200
+            else:
+                return {
+                    "errors": {
+                        "message": "You must be the owner of the server to delete this channel"
+                    }
+                }, 401
+        else:
+            return {"errors": {"message": "Channel not found"}}, 404
     else:
-        return {"errors": {"message": "Channel not found"}}, 404
+        return {"errors": {"message": "Unauthorized"}}, 401
 
 
 @channel_routes.route("/<int:id>/messages")
@@ -103,7 +106,6 @@ def get_channel_messages(id):
     if not channel:
         return {"errors": {"message": "Channel not found"}}, 404
 
-    # Check if user is member of server that owns this channel
     server_member = ServerMember.query.filter(
         ServerMember.server_id == channel.server_id,
         ServerMember.user_id == current_user.id,
