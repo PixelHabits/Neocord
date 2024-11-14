e24d742d0100d4c4318e886f8199646b78203475
Christian May <christianmay@Christians-Air.attlocal.net>
2024-11-14 15:42:45 -0600

    refactor(nav bar): add join server button if user is logged in

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -1,23 +1,26 @@
 import { NavLink } from "react-router-dom";
 import { ProfileButton } from "./ProfileButton.tsx";
 import "./Navigation.css";
-import { useStore } from "../../store/store.ts";
+// import { useStore } from "../../store/store.ts";
 
 
 function Navigation() {
-  const user = useStore((state) => state.user);
+  // const user = useStore((state) => state.user);
+  const user = true;
   
   return (
-    <ul className="flex items-center justify-between p-4 align-middle text-white">
-      <li className="text-3xl">
-        <NavLink to="/">Home</NavLink>
+    <ul className="flex items-center p-4 text-white">
+      <li>
+        <NavLink className="text-3xl" to="/">Home</NavLink>
       </li>
        
-      <li className="text-3xl pt-2 flex items-center">
+      <li className="flex ml-auto">
         {user && (
-          <NavLink className="text-blue-500 text-xl mr-4" to="/login">Join a server!</NavLink>
+          <NavLink className="text-blue-500 text-xl" to="/login">Join a server!</NavLink>
         )}
-        <ProfileButton/>
+      </li>
+      <li className="text-4xl">
+        <ProfileButton />
       </li>
     </ul>
   );
