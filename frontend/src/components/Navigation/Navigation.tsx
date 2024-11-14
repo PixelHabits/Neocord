e69a5c156a33c29268365cadb2ec75123067f9d2
Christian May <christianmay@Christians-Air.attlocal.net>
2024-11-14 15:15:07 -0600

    refactor(nav bar): add styling to navigation bar

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -1,16 +1,23 @@
 import { NavLink } from "react-router-dom";
 import { ProfileButton } from "./ProfileButton.tsx";
 import "./Navigation.css";
+import { useStore } from "../../store/store.ts";
+
 
 function Navigation() {
+  const user = useStore((state) => state.user);
+  
   return (
-    <ul>
-      <li>
+    <ul className="flex items-center justify-between p-4 align-middle text-white">
+      <li className="text-3xl">
         <NavLink to="/">Home</NavLink>
       </li>
-
-      <li>
-        <ProfileButton />
+       
+      <li className="text-3xl pt-2 flex items-center">
+        {user && (
+          <NavLink className="text-blue-500 text-xl mr-4" to="/login">Join a server!</NavLink>
+        )}
+        <ProfileButton/>
       </li>
     </ul>
   );
