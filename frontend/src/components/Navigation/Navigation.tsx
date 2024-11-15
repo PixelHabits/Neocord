b10ac94a4bcb6de73cedf2b44d0c66f5ef0c20c8
Christian <31420801+Mayboyzz@users.noreply.github.com>
2024-11-14 23:21:36 -0600

    Fix/Nav Bar (#16)
    * refactor(nav bar): add demo user button
    
    * fix(nav bar): Fix logout button not showing when user is logged in, fix styling on nav bar component

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -5,22 +5,22 @@ import { useStore } from "../../store/store.ts";
 
 
 function Navigation() {
-  const user = useStore((state) => state.user);
+  // const user = useStore((state) => state.user);
+  const user = {
+    username: "test",
+    email: "test@test.com"
+  }
   
   return (
     <ul className="flex items-center p-4 text-white">
       <li>
-        <NavLink className="text-3xl" to="/">Home</NavLink>
+        <NavLink className="text-3xl" to="/">Neocord</NavLink>
       </li>
-       
-      <li className="flex ml-auto">
-        {user && (
-          <NavLink className="text-blue-500 text-xl" to="/login">Join a server!</NavLink>
-        )}
-      </li>
-      <li className="text-4xl">
-        <ProfileButton />
-      </li>
+      {user && (
+        <li className="text-4xl ml-auto">
+          <ProfileButton />
+        </li>
+      )}
     </ul>
   );
 }
