6b8f0c91301e4c0784dbb17a328cba3cb06572f0
Christian May <mayboyz99@gmail.com>
2024-11-17 19:59:20 -0600

    Feature(Reactions): Implement Partial CRUD Operations for Message Reactions
    - Add reaction button to append new reactions to messages
    - Implement reaction removal functionality when clicking existing reactions
    - Display reaction emoji and count in message interface

--- frontend/src/components/ChatBox/components/MessageItem.tsx
+++ frontend/src/components/ChatBox/components/MessageItem.tsx
@@ -1,11 +1,52 @@
+import { useStore } from '../../../store/store.ts';
 import type { Message } from '../../../types/index.ts';
-
 export const MessageItem = ({ message }: { message: Message }) => {
+	const { removeReaction, addReaction } = useStore();
+
+	const handleRemoveReactionClick = (messageId: number, reactionId: number) => {
+		removeReaction(messageId, reactionId);
+	};
+
+	const handleAddReactionClick = (
+		messageId: number,
+		reaction: { emoji: string },
+	) => {
+		addReaction(messageId, reaction);
+	};
+
 	return (
 		<div>
 			<h4>{message.user?.username}</h4>
-			<div className='flex bg-neutral-800/80 px-4 py-8 text-neutral-400 shadow-md'>
+			<div className='flex flex-col bg-neutral-800/80 px-4 py-8 text-neutral-400 shadow-md'>
 				<span>{message.body}</span>
+				<div className='flex gap-2'>
+					{message.reactions.map((reaction) => {
+						return (
+							<div
+								key={reaction.id}
+								onClick={() =>
+									handleRemoveReactionClick(message.id, reaction.id)
+								}
+								onKeyDown={(e) => {
+									if (e.key === 'Enter') {
+										handleRemoveReactionClick(message.id, reaction.id);
+									}
+								}}
+								className='mt-2 flex w-fit items-center gap-2 rounded-md bg-neutral-700/80 px-2 py-1 text-xs hover:cursor-pointer hover:bg-neutral-700/90'
+							>
+								<span className='cursor-pointer'>{reaction.emoji}</span>
+								<span>{1}</span>
+							</div>
+						);
+					})}
+					<button
+						className='mt-2 w-fit rounded-md bg-neutral-700/80 px-2 py-1 text-xs hover:cursor-pointer hover:bg-neutral-700/90'
+						type='button'
+						onClick={() => handleAddReactionClick(message.id, { emoji: '👍' })}
+					>
+						+
+					</button>
+				</div>
 				{/* <button onClick={() => handleDeleteMessage(msg.id)}>Delete</button> */}
 			</div>
 			<span className='text-neutral-400 text-xs italic'>
